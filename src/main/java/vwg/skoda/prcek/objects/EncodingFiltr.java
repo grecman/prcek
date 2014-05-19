f04f5c026d3ae8fc71a31c1ba0c8462965ec8d66
dzc0grp <petr.grecman@skoda-auto.cz>
2014-05-19 14:37:09 +0200

    1. pom.xml - vytvareni EARu !!!!! 2. EncodingFilter - prechod na springove reseni.

--- src/main/java/vwg/skoda/prcek/objects/EncodingFiltr.java
+++ src/main/java/vwg/skoda/prcek/objects/EncodingFiltr.java
@@ -11,24 +11,7 @@ import javax.servlet.ServletResponse;
 
 public class EncodingFiltr implements Filter {
 
-	/*
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
-	// Encoding filtr pro naSETotovani requestu na UTF-8
-	// Aktivuje a definuje (mapovani na url) se ve WEB.XML
-	// Mozna to na jinem serveru (nez na mem lokalnim TOMCATu) bude normalne fungovat i bez tohoto filtru!
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		request.setCharacterEncoding("UTF-8");
-		chain.doFilter(request, response);
-	}
-
-	@Override
-	public void destroy() {
-	}
-	*/
-
+	//Tato trida byla implementovana ve web.xml, ale nahradil jsem ji SPRINGovou variatnou a tim padem jiz tato trida nadale neni potreba!
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
